2bd87cedbbbec45a37a560f0ed32be3979457831
Sven Boemer <sbomer@gmail.com>
2019-09-05 22:19:40 +0000

    Turn off https redirection

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -36,8 +36,6 @@ namespace webapi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
