151e628eb47e9d626e6e2d7016ab8ae42f9604e6
Sven Boemer <sbomer@gmail.com>
2019-09-05 22:26:09 +0000

    Measure startup using stopwatch

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -14,11 +15,16 @@ namespace webapi
     {
         public static async Task Main(string[] args)
         {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             using (var host = CreateHostBuilder(args).Start())
             {
                 var client = new HttpClient();
                 var response = await client.GetAsync("http://localhost:5000/WeatherForecast");
                 response.EnsureSuccessStatusCode();
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                Console.WriteLine("STARTUP TIME: " + ts.Milliseconds);
             }
         }
 
