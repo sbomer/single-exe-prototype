680d0942fa1612152b78f60630e20998a65d7ac1
Sven Boemer <sbomer@gmail.com>
2019-09-06 00:20:45 +0000

    Add repro script

---
+++ repro.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# apt install cmake clang llvm libicu-dev liblttng-ust-dev libkrb5-dev
+#
+# pushd coreclr
+# export LANG=en_US.UTF-8
+# ./build.sh -stripsymbols
+# popd
+#
+# wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/release/3.0.1xx/dotnet-sdk-latest-linux-x64.tar.gz
+# tar xf dotnet-sdk-latest-linux-x64.tar.gz
+
+dotnet publish -c Release webapi/webapi.csproj /v:n
+mkdir -p bin
+pushd bin
+dotnet ../coreclr/bin/Product/Linux.x64.Release/bundle.dll --source ../publish --host webapi
+popd
